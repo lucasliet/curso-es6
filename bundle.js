91f81eb05e3f2b0be12335b8fe0a672b38f4ea42
lucasliet <lucasouliveira@gmail.com>
2020-04-17 16:06:19 -0300

    find

--- bundle.js
+++ bundle.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var arr = [1, 3, 4, 5, 8, 9];
+var arr = [1, 9, 3, 4, 5, 8, 9];
 var newArr = arr.map(function (item, index) {
   return {
     valor: item,
@@ -16,3 +16,7 @@ var pares = arr.filter(function (item) {
   return item % 2 === 0; //condição do item a ser retornado
 });
 console.log(pares);
+var encontrar = arr.find(function (item) {
+  return item === 9; //retorna '9' se ele existir no array
+});
+console.log(encontrar);
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const arr = [ 1, 3, 4, 5, 8, 9 ];
+const arr = [ 1, 9, 3, 4, 5, 8, 9 ];
 
 const newArr = arr.map(function(item, index){
     return { valor:item, posicao:index};
@@ -8,12 +8,18 @@ console.log(newArr);
 
 const somarTodosItens = arr.reduce(function(total, next){
     return total + next;
-})
+});
 
 console.log(somarTodosItens);
 
 const pares = arr.filter(function(item){
     return item % 2 === 0; //condição do item a ser retornado
-})
+});
 
 console.log(pares);
+
+const encontrar = arr.find(function(item){
+    return item === 9; //retorna '9' se ele existir no array
+});
+
+console.log(encontrar);
